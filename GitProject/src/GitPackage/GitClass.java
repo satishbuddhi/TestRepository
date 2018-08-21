4c4e6fd8d4cb2abed9515c24be3c30d391df35c6
satish <satishbujji03@gmail.com>
2018-08-21 16:08:31 +0530

    fifth commit from local repo

--- GitProject/src/GitPackage/GitClass.java
+++ GitProject/src/GitPackage/GitClass.java
@@ -6,5 +6,6 @@ public static void main(String[] args) {
 	System.out.println("World");
 	//adding comment line
 	System.out.println("second upload to remote from local repo");
+	System.out.println("third upload to remote from local repo");
 }
 }
